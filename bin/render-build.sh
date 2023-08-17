48a5bdd3f928fa012f7c234618cf9da08a6bc776
sinagaki <soma0822soma@gmail.com>
2023-08-17 07:13:33 +0000

    test

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,6 @@
 # exit on error
 set -o errexit
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
