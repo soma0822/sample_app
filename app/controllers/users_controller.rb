20a036ccb700b69d9b2172c25118237fc869bf06
sinagaki <soma0822soma@gmail.com>
2023-08-18 02:37:45 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,15 +6,16 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-    def create
-    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+  def create
+    @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
   end
-  
+
   private
 
     def user_params
