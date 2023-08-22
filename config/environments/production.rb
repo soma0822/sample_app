0e41e7e4ae415568eb4d84ad7425785a8011fc55
sinagaki <soma0822soma@gmail.com>
2023-08-22 07:24:49 +0000

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'sample-app-8o5n.onrender.com/'
+  host = 'https://sample-app-8o5n.onrender.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
