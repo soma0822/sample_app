422a6ed273613a587051f9b9902ef3280b929bf7
sinagaki <soma0822soma@gmail.com>
2023-08-23 01:22:57 +0000

    12.3.3

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -12,6 +12,8 @@ class UserMailerPreview < ActionMailer::Preview
   # Preview this email at
   # https://38da3cf6cdc74283867901de9cc9f21a.vfs.cloud9.ap-northeast-1.amazonaws.com/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 end
